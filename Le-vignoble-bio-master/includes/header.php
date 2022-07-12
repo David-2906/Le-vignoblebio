f1eb0c63c89d31985efd67fed1beddf729d574c2
David <david.pires2906@gmail.com>
2022-07-12 10:05:12 +0200

    deuxieme

---
+++ Le-vignoble-bio-master/includes/header.php
@@ -0,0 +1,58 @@
+<header>
+    <div class="header2 bg-success-gradiant">
+      <div class="">
+
+        <nav class="navbar navbar-expand-lg h2-nav">
+          <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header2" aria-controls="header2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-menu"></span>
+          </button>
+          <div class="collapse navbar-collapse hover-dropdown" id="header2">
+            <ul class="navbar-nav">
+              <li class="nav-item active"><a class="nav-link" href="#"><h1>Le Vignoble bio</h1></a></li>
+              <img src="images/logo.png" alt="Une photo de notre logo">
+              <li class="nav-item dropdown position-relative">
+                <a class="nav-link dropdown-toggle" href="#" id="h2-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Découvrez nos vins français<i class="fa fa-angle-down ml-1 font-12"></i>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="nos-vins-rouge.html">Nos vins rouges</a></li>
+                  <li><a class="dropdown-item" href="nos-vins-blanc.html">Nos vins blancs</a></li>
+                  <li><a class="dropdown-item" href="nos-vins-rose.html">Nos vins rosés</a></li>
+                  <li class="divider" role="separator"></li>
+                  <li><a class="dropdown-item" href="#">Nos coffrets</a></li>
+                  <li class="divider" role="separator"></li>
+                  <li><a class="dropdown-item" href="#">Nos promotions</a></li>
+                  <li class="dropdown-submenu position-relative"> <a class="dropdown-toggle dropdown-item" data-toggle="dropdown" href="#">Dropdown <i class="fa fa-long-arrow-right float-right mt-1"></i></a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li class="dropdown-submenu position-relative"> <a class="dropdown-toggle dropdown-item" href="#" data-toggle="dropdown">Dropdown <i class="fa fa-long-arrow-right float-right mt-1"></i></a>
+                        <ul class="nav navbar-nav dropdown-menu">
+                          <li class="dropdown-submenu position-relative"> <a class="dropdown-toggle dropdown-item" href="#" data-toggle="dropdown">Dropdown <i class="fa fa-long-arrow-right float-right mt-1"></i></a>
+                            <ul class="dropdown-menu">
+                              <li><a class="dropdown-item" href="#">Action</a></li>
+                              <li><a class="dropdown-item" href="#">Another action</a></li>
+                              <li><a class="dropdown-item" href="#">Something else here</a></li>
+                              <li class="divider" role="separator"></li>
+                              <li><a class="dropdown-item" href="#">Separated link</a></li>
+                              <li class="divider" role="separator"></li>
+                              <li><a class="dropdown-item" href="#">One more separated link</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item"><a class="nav-link" href="Qui-sommes-nous.php">Qui sommes nous</a></li>
+              <li class="nav-item"><a class="nav-link" href="#">Contactez-nous</a></li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Espace professionnel</a></li>
+              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Inscrivez-vous</a></li>
+              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Connectez-vous</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
