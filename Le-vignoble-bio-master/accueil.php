0af678d4c697def4bccb1ee47efdcff740b3e2f4
David <david.pires2906@gmail.com>
2022-07-13 16:29:40 +0200

    Inscription

--- Le-vignoble-bio-master/accueil.php
+++ Le-vignoble-bio-master/accueil.php
@@ -19,41 +19,44 @@
     ?>    
         <!-- End Header 1 code -->
 
+        <div class="title"><h2>Bienvenue sur Le Vignoble bio</h2></div>
 
+        
   
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="images/pexels-luiz-m-santos-760280.jpg" width="200" height="700" alt="Une photo de grappes de raisin">
+            <img class="d-block w-100" src="images/pexels-luiz-m-santos-760280.jpg"  alt="Une photo de grappes de raisin">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="images/pexels-pixabay-434311.jpg" width="200" height="700" alt="Une photo de la fabrication dans la cuve à vin">
+            <img class="d-block w-100" src="images/pexels-pixabay-434311.jpg"  alt="Une photo de la fabrication dans la cuve à vin">
           </div>
           <div class="carousel-item">
-          <img class="d-block w-100" src="images/pexels-fotograf-jylland-2995283.jpg" width="700" height="700" alt="Une photo de cave à vin">
+          <img class="d-block w-100" src="images/pexels-fotograf-jylland-2995283.jpg"  alt="Une photo de cave à vin">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="images/pexels-engin-akyurt-3620231.jpg" width="700" height="700" alt="Une photo d'un homme servant du vin">
+            <img class="d-block w-100" src="images/pexels-engin-akyurt-3620231.jpg"  alt="Une photo d'un homme servant du vin">
             </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="images/pexels-cottonbro-3403505.jpg" width="700" height="700" alt="une photo d'exposition de bouteille de vins ">
+            <img class="d-block w-100" src="images/pexels-cottonbro-3403505.jpg"  alt="une photo d'exposition de bouteille de vins ">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="images/pexels-brett-sayles-2606387.jpg" width="700" height="700" alt="une photo d'un verre de vin ">
+            <img class="d-block w-100" src="images/pexels-brett-sayles-2606387.jpg"  alt="une photo d'un verre de vin ">
           </div>
             <div class="carousel-item">
-            <img class="d-block w-100" src="images/pexels-brett-sayles-1374552.jpg" width="700" height="700" alt="Une photo d'une célébration d'une grande occasion avec verres de vin ">
+            <img class="d-block w-100" src="images/pexels-brett-sayles-1374552.jpg"  alt="Une photo d'une célébration d'une grande occasion avec verres de vin ">
           </div>
           <div class="carousel-item">
-          <img class="d-block w-100" src="images/pexels-adrien-olichon-2537608.jpg" width="700" height="700" alt="une photo d'une célébration avec verres de vin ">
+          <img class="d-block w-100" src="images/pexels-adrien-olichon-2537608.jpg"  alt="une photo d'une célébration avec verres de vin ">
           </div>
           </div>
         </div>
       </div>
 
+
       <div class='container mx-auto mt-5 col-md-10 mt-100'>
         <div class="header">
-            <div class="title"><h2>Bienvenue sur Le Vignoble bio.</h2></div>
+         
             <p><small class="text-muted"><h2>Pour vous guidez, voici quelques informations importantes :</h2><br /></small></p>
         </div>
         <div class="row" style="justify-content: center">
@@ -101,6 +104,8 @@
             </div>
         </div>
     </div>
+
+
         <?php
         include 'includes/footer.php';
         ?>
--- Le-vignoble-bio-master/includes/header.php
+++ Le-vignoble-bio-master/includes/header.php
@@ -33,9 +33,45 @@
             </ul>
             <ul class="navbar-nav ml-auto">
               <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Espace professionnel</a></li>
-              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Inscrivez-vous</a></li>
-              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Connectez-vous</a></li>
+              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#" data-toggle="modal" data-target="#exampleModal">Inscrivez-vous</a></li>
+              <li class="nav-item"><a class="btn rounded-pill btn-dark py-2 px-4" href="#">Mon compte</a></li>
             </ul>
           </div>
         </nav>
+
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Inscription</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <form class="p-3" action="#" method="post">
+                <div class="row mb-3">
+                    <label class="col-12 col-md-3 pl-0" for="firstname">Prénom</label>
+                    <input class="col-12 col-md-9" type="text" name="firstname" id="firstname" placeholder="Prénom">
+                </div>
+                <div class="row mb-3">
+                    <label class="col-12 col-md-3 pl-0" for="name">Nom</label>
+                    <input class="col-12 col-md-9" type="text" name="name" id="name" placeholder="Nom">
+                </div>
+                <div class="row mb-3">
+                    <label class="col-12 col-md-3 pl-0" for="email">Email</label>
+                    <input class="col-12 col-md-9" type="email" name="email" id="email" placeholder="Adresse email">
+                </div>
+                <div class="row mb-3">
+                    <label class="col-12 col-md-3 pl-0" for="password">Mot de passe</label>
+                    <input class="col-12 col-md-9" type="password" name="password" id="password" placeholder="Mot de passe">
+                </div>
+                <input type="hidden" name="action" value="signup">
+                <div class="row mb-3 justify-content-end">
+                    <input  class="" type="submit" value="Valider">
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer h-0">
       </header>
